a45f7d6893df205feb1baa2ca41d694ba44e0a84
ashifanassar <ashifanassar@gmail.com>
2024-05-02 19:01:00 +0530

    module

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "rabbitmqdb" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "rabbitmqdb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
+  zone_id = "Z0547409165EGAKUG3EH3"
+  name    = "rabbitmqdb-${var.ENV}.roboshopshopping"
   type    = "CNAME"
   ttl     = 10
   records = [aws_spot_instance_request.rabbitmqdb.private_ip]
